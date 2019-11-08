e7975d59123bc779eae5f2e6e4162119bf6fa223
Kelvin Lopes <kelvin7android@gmail.com>
2019-11-07 21:31:41 -0300

    Atualização para HelpOrderController

--- src/app/controllers/HelpOrderController.js
+++ src/app/controllers/HelpOrderController.js
@@ -42,12 +42,6 @@ class HelpOrderController {
         answer_at: null,
       },
     });
-
-    if (!listHelpOrders || listHelpOrders.answer_at !== null) {
-      return res.status(400).json({
-        error: 'You have no new help orders.',
-      });
-    }
     return res.json(listHelpOrders);
   }
 }
